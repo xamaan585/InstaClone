09d1898cc724fdd7d2befa93343f87a2584d7eb9
Abdirahman <dubowhamaan@gmail.com>
2022-03-05 12:09:29 +0300

    updating settings.py

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'cloudinary',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
